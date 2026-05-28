eeea5da510118202f3d92f5ea832b48284e21904
salmaCal <salmabalmir55@gmail.com>
2026-05-28 22:22:42 +0100

    commit 3

--- src/app/products/products.component.ts
+++ src/app/products/products.component.ts
@@ -1,12 +1,28 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { NgFor, NgIf } from '@angular/common'; // 1. On importe NgFor (plus simple)
 
 @Component({
   selector: 'app-products',
-  imports: [],
+  imports: [
+    NgIf,
+    NgFor // 2. On utilise NgFor ici
+  ],
   templateUrl: './products.component.html',
   styleUrl: './products.component.css',
   standalone: true,
 })
-export class ProductsComponent {
+export class ProductsComponent implements OnInit {
 
+  // 3. Typage plus précis pour vos produits
+  products: Array<any> = [];
+
+  constructor () {}
+
+  ngOnInit() {
+    this.products = [
+      { id: 1, name: "Computer", price: 2300, selected: true },
+      { id: 2, name: "Printer", price: 1200, selected: false },
+      { id: 3, name: "Smart Phone", price: 1100, selected: true },
+    ];
+  }
 }
