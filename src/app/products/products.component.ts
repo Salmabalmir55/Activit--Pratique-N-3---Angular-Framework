9a5a048b01cb29e62626b6e4fd924cc40951cf64
salmaCal <salmabalmir55@gmail.com>
2026-05-28 22:50:56 +0100

    commit 3

--- src/app/products/products.component.ts
+++ src/app/products/products.component.ts
@@ -1,11 +1,10 @@
 import { Component, OnInit } from '@angular/core';
-import { NgFor, NgIf } from '@angular/common'; // 1. On importe NgFor (plus simple)
+import { NgFor, NgIf } from '@angular/common';
 
 @Component({
   selector: 'app-products',
   imports: [
-    NgIf,
-    NgFor // 2. On utilise NgFor ici
+
   ],
   templateUrl: './products.component.html',
   styleUrl: './products.component.css',
@@ -16,13 +15,21 @@ export class ProductsComponent implements OnInit {
   // 3. Typage plus précis pour vos produits
   products: Array<any> = [];
 
-  constructor () {}
+  constructor() {
+  }
 
   ngOnInit() {
     this.products = [
-      { id: 1, name: "Computer", price: 2300, selected: true },
-      { id: 2, name: "Printer", price: 1200, selected: false },
-      { id: 3, name: "Smart Phone", price: 1100, selected: true },
+      {id: 1, name: "Computer", price: 2300, selected: true},
+      {id: 2, name: "Printer", price: 1200, selected: false},
+      {id: 3, name: "Smart Phone", price: 1100, selected: true}
     ];
+  }
+
+  handleDelete(product: any) {
+    let v = confirm("Are you sure you want to delete this product?");
+    if (v == true) {
+      this.products = this.products.filter((p) => p.id != product.id);
+    }
   }
 }
